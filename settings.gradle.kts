d4a069eb3c82ba6f7f576a208b3f32bcbb5f460d
Peanut0220 <151499166+Peanut0220@users.noreply.github.com>
2024-12-01 21:37:57 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+val username = "paypal_sgerritz"
+val password = "AKCp8jQ8tAahqpT5JjZ4FRP2mW7GMoFZ674kGqHmupTesKeAY2G8NcmPKLuTxTGkKjDLRzDUQ"
 pluginManagement {
     repositories {
         google()
@@ -15,8 +17,8 @@ dependencyResolutionManagement {
         maven{
             url = uri("https://cardinalcommerceprod.jfrog.io/artifactory/android")
             credentials{
-                username ="paypal_sgerritz"
-                password = "AKCp8jQ8tAahqpT5JjZ4FRP2mW7GMoFZ674kGqHmupTesKeAY2G8NcmPKLuTxTGkKjDLRzDUQ"
+                username =username
+                password = password
             }
         }
         maven {
